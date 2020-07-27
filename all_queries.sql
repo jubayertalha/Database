742aae0e829a8c2e7f06dfdba2429e103708a4c9
Talha Jubayer <jubayertalha194@gmail.com>
2020-07-27 13:11:38 +0600

    add lecture 6

--- all_queries.sql
+++ all_queries.sql
@@ -55,3 +55,38 @@ SELECT ename AS "Name" FROM emp WHERE ename LIKE '%L%L%' AND ( deptno = 30 OR mg
 
 12-
 SELECT ename AS "Name", job AS "Job", sal AS "Salary" FROM emp WHERE job IN('CLERK', 'ANALYST') AND sal NOT IN(1000,3000,5000);
+
+
+
+
+----------------------Lecture 6-------------------------------------------------
+
+1-
+SELECT SYSDATE AS "Date" FROM DUAL;
+
+2-
+SELECT empno AS "Employee Number", ename AS "Name", sal AS "Salary", ROUND(sal+(sal*0.15)) AS "New Salary" FROM emp;
+
+3-
+SELECT empno AS "Employee Number", ename AS "Name", sal AS "Salary", ROUND(sal+(sal*0.15)) AS "New Salary", ROUND(sal+(sal*0.15))-sal AS "Increase" FROM emp;
+
+4-
+SELECT ename AS "Name", hiredate AS "Hire Date", TO_CHAR(NEXT_DAY(ADD_MONTHS(hiredate,6),'MONDAY'), 'Day, "the" Ddspth "of" Month, YYYY.') AS "REVIEW" FROM emp;
+
+5-
+SELECT ename AS "Name", ROUND(MONTHS_BETWEEN(SYSDATE, hiredate)) AS "MONTHS_WORKED" FROM emp ORDER BY "MONTHS_WORKED" DESC;
+
+6-
+SELECT ename ||' earns ' || sal || ' monthly but wants ' || sal*3 AS "Dream Salaries" FROM emp;
+
+7-
+SELECT INITCAP(ename) AS "Name", LENGTH(ename) AS "Length" FROM emp WHERE SUBSTR(LOWER(ename),1,1) IN('j','a','m');
+
+8.1-
+SELECT ename AS "Name", NVL(TO_CHAR(comm), 'No Commission.') AS "COMM" FROM emp;
+
+8.2-
+SELECT ename AS "Name", DECODE(comm, NULL, 'No Commission.', 0, 'No Commission', comm) AS "COMM" FROM emp;
+
+9-
+SELECT ename || '    ' || RPAD('*',ROUND(sal/100),'*') AS "EMPLOYEE_AND_THEIR_SALARIES" FROM emp ORDER BY sal DESC;
